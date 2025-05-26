824b87408c972ec23237736dd0d6a74a27e1dd0a
webermayank <111176033+webermayank@users.noreply.github.com>
2025-05-26 14:22:00 +0530

    added build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import routes from './routes/index.js';
-
+import express from "express";
+import routes from "./routes/index.js";
 
 const app = express();
 app.use(express.json());
-app.use('/api', routes);
+app.use("/api", routes);
 
 export default app;
