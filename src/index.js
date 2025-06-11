69d897f7429dc309049b153d57f6a0223e74dcc8
webermayank <111176033+webermayank@users.noreply.github.com>
2025-06-12 01:07:42 +0530

    bugs in index

--- src/index.js
+++ src/index.js
@@ -6,89 +6,7 @@ import path from "path";
 import routes from "./routes/index.js";
 import mongoose from "mongoose";
 import cors from "cors";
-import 
-const app = express();
-app.use(express.json());
-
-const mongoURI = process.env.MONGO_URI;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-    process.exit(1);
-  });
-
-app.use(helmet());
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-
-const uploadsPath = path.join(process.cwd(), "uploads");
-app.use("/uploads", express.static(uploadsPath));
-
-app.use("/api", routes);
-
-export default app;
-
-                "data:",
-            ],
-            workerSrc: [
-                "'self'",
-                "blob:",
-            ],
-            connectSrc: ["'self'"],
-            frameSrc: ["'self'"],
-        },
-    })
-); 
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
-  );
-  next();
-});
-
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-
-const uploadsPath = path.join(process.cwd(), "uploads");
-app.use("/uploads", express.static(uploadsPath));
-
-app.use("/api", routes);
-
-export default app;
-
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import { fileURLToPath } from "url";
-import path from "path";
-import routes from "./routes/index.js";
-import mongoose from "mongoose";
-import cors from "cors";
+import helmet from "helmet";
 import helmet from "../helmetconfig.js";
 
 const app = express();
@@ -99,47 +17,47 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(
-  cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-    credentials: true,
-  })
+    cors({
+        origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+        credentials: true,
+    })
 );
 
 mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-    process.exit(1);
-  });
+    .connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("Error connecting to MongoDB", err);
+        process.exit(1);
+    });
 
-app.use(helmet());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
-  );
-  next();
-});
-
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-
-const uploadsPath = path.join(process.cwd(), "uploads");
-app.use("/uploads", express.static(uploadsPath));
-
-app.use("/api", routes);
-
-export default app;
-
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: [
+                "'self'",
+                "https://buttons.github.io",
+            ],
+            styleSrc: [
+                "'self'",
+                "'unsafe-inline'",
+            ],
+            fontSrc: [
+                "'self'",
+                "data:",
+            ],
+            imgSrc: [
+                "'self'",
+                "data:",
+            ],
+            mediaSrc: [
+                "'self'",
                 "data:",
             ],
             workerSrc: [
@@ -150,20 +68,19 @@ export default app;
             frameSrc: ["'self'"],
         },
     })
-); 
-
+);
 app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
-  );
-  next();
+    res.setHeader(
+        "Content-Security-Policy",
+        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+    );
+    next();
 });
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 const uploadsPath = path.join(process.cwd(), "uploads");
