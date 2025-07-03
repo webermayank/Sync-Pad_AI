214b7e1aaecb9285c79dbd9fe2e7213519d990d0
webermayank <111176033+webermayank@users.noreply.github.com>
2025-07-03 18:16:38 +0530

    packages , CSP, viteConfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,10 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
+      "/api":{
+        target :"http://localhost:8080",
+        changeOrigin: true,
+        secure: false,}
     },
   },
 });
