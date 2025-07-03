397967cabac96d598332f8aca11b3d7ab39de8d6
webermayank <111176033+webermayank@users.noreply.github.com>
2025-07-03 18:20:47 +0530

    landing page and editor has same css file

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -3,14 +3,20 @@ import LandingFeatures from "../components/EditorSection";
 import FeatureCarousel from "../components/FeatureCarousel";
 import ContributeSection from "../components/ContributeSection";
 import Footer from "../components/Footer";
-
+import WorkflowSection from "../components/WorkflowSection";
+import FeatureCards from "../components/FeatureSection";
 const Landing: React.FC = () => {
   return (
     <div className="landing-page">
       <LandingHero />
       <LandingFeatures />
       <FeatureCarousel />
+      <WorkflowSection />
+      <FeatureCards />
+      
+      {/* Contribute Section */}
       <ContributeSection/>
+      
       <Footer />
     </div>
   );
